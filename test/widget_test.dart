00a0e0c2fda23710bd527442bc55d9ea7d1451b9
Jordan King <jrodriguezm216@pichincha.pe>
2021-05-28 07:25:18 -0500

    first prototipe p

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dvida/main.dart';
+import 'package:dvida/main_origin.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
